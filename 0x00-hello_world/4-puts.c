e1b36595dfc635df4a5dc0fe55fd2823fde03c5f
Estihame <estihame@gmail.com>
2023-06-17 19:11:33 -0700

    Commiting changes to first C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/**
-*Using the puts function
-*/
-int main(void) 
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
