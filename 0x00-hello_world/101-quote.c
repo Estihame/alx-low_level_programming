183a39dd566b1bf55d9c657c9b569dae09bdb586
Estihame <estihame@gmail.com>
2023-06-19 03:49:27 -0700

    Checking for errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point for this code
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char meg;
+
+	for (meg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	putchar (meg);
+	{
+	return (1);
+}
+}
