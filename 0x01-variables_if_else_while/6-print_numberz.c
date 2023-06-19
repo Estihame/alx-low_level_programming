5949989557a528a343f00ca1850a59482164294d
Estihame <estihame@gmail.com>
2023-06-19 13:07:17 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point for this code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char baseten;
+
+	for (baseten = '0'; baseten <= '9'; baseten++)
+{
+	putchar(baseten);
+}
+	putchar('\n');
+	return (0);
+}
