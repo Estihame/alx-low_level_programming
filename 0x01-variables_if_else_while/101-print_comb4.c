f4f832caa4dcfa61b5f1e8f081875dabbb0f93b7
Estihame <estihame@gmail.com>
2023-06-19 19:28:58 -0700

    Remove last coma trace

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,16 +8,17 @@ int main(void)
 {
 int firstnum, secondnum, thirdnum;
 	
-	for (firstnum = 0; firstnum <= 9; firstnum++)
+	for (firstnum = 0; firstnum <= 7; firstnum++)
 {
-	for (secondnum = firstnum + 1; secondnum <= 9; secondnum++)
+	for (secondnum = firstnum + 1; secondnum <= 8; secondnum++)
 {
 	for (thirdnum = secondnum + 1; thirdnum <= 9; thirdnum++)
 {
 	putchar('0' + firstnum);
 	putchar('0' + secondnum);
 	putchar('0' + thirdnum);
-	if (firstnum != 8 || secondnum != 9 || thirdnum != 10)
+
+	if (firstnum != 7 || secondnum != 8 || thirdnum != 9)
 {
 	putchar(',');
 	putchar(' ');
