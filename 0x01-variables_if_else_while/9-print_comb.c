85ba453d6a0c2d1b15a8e0ed088952de959a56a7
Estihame <estihame@gmail.com>
2023-06-19 18:17:59 -0700

    Adding print combo file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point for this code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int printcombo;
+
+	for (printcombo = 0; printcombo <= 9; printcombo++)
+	{
+	putchar(printcombo);
+	}
+	if (printcombo != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+	putchar('\n');
+
+	return (0);
+}
