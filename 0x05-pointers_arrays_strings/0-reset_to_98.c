3fedf83b65c39a2b1aed8ea1c99ab0ac9e58bed3
Estihame <estihame@gmail.com>
2023-06-26 07:32:32 -0700

    Adding the file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+
 /*
- * reset_to_98 - Resting Array value to 98
+ * reset_to_98 function - as parameter  Resting Array value to 98
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	putchar (*n);
-	putchar('\n');
 }
