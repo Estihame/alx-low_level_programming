8b8edc7ff86cce519f3c027a07fc95621c42df3c
Estihame <estihame@gmail.com>
2023-06-30 14:32:29 -0700

    Adding first file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,29 @@
-#include <stdio>
+#include <stdio.h>
 #include "main.h"
+
+/**
+ * *_strcat - Entry point of this code
+ *@dest: Element of code
+ *@src: Element of code
+ * Return: - Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+int a;
+int b;
+
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+b = 0;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
