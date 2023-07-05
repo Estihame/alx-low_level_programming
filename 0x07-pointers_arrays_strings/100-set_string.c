9b6b6bd3036b08ea90f57847915444b8af7f86de
Estihame <estihame@gmail.com>
2023-07-04 20:44:13 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *set_string - copies n from memory area
+ *@s: Number to input
+ *@to: input
+ *Return: Always 0
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
