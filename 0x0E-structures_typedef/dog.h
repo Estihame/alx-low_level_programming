817290b5bb8e99a6a777c276b183a5a9f36e99f9
Estihame <estihame@gmail.com>
2023-07-18 07:04:10 -0700

    Adding Poppy File

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First element
+ * @age: Second element
+ * @owner: Third element
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
