9de8c9d7e3e684bf376522041f5ed228718493fe
Estihame <estihame@gmail.com>
2023-07-22 23:59:11 +0000

    Adding 2-print_strings.c file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,12 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+/*
+ * print_numbers - Function prints numbers and a new line
+ * @seperator: string to bbe printed between numbers
+ * @n: Number of integers passed
+ * @...: A set of numbers to be printed
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list dig;
